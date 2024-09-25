1e25ed6f01a960280462e9280c0db855a51af036
ashleyrojas <ashleycr33@gmail.com>
2024-09-25 11:43:38 -0600

    refactor: changes in layout styles for a better ui

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,10 +7,17 @@
     <title>Suru Admin</title>
     @vite('resources/css/app.css')
 </head>
-<body class="bg-[#EFF0F8]">
-    @include('partials.nav')
-    <section class="max-w-[90%] mx-auto">
+<body class="flex flex-row font-main bg-[#EFF0F8]">
+    <div
+        class="fixed flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 h-[100vh] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+        <div class="flex justify-center items-center p-4">
+            <img class="m-4 h-8" src="{{ asset('icons/logo.svg') }}" alt="Logo">
+        </div>
+        @include('partials.nav')
+    </div>
+    <div class="w-full ml-[20rem] p-5">
         @yield('content')
-    </section>
+        <h1>AAAAAA</h1>
+    </div>
 </body>
 </html>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,12 +1,27 @@
-<nav class="bg-white justify-between w-full flex px-16 text-clr-white items-center shadow-md">
-    <span>Suru</span>
-    <section class="flex gap-4 duration-150">
-        <a href="#" class="p-3 hover:bg-gray-300 h-full cursor-pointer">Properties</a>
-        <a href="#" class="p-3 hover:bg-gray-300 h-full cursor-pointer">Users</a>
-        <a href="#" class="p-3 hover:bg-gray-300 h-full cursor-pointer">Appointments</a>
-        <a href="#" class="p-3 hover:bg-gray-300 h-full cursor-pointer">Partners</a>
-        @if (session()->has('user'))
-            <a href="{{ route('admin.logout') }}" class="p-3 hover:bg-gray-300 h-full cursor-pointer">Logout</a>
-        @endif
-    </section>
+<?php
+$links = array(
+    array("name" => "Properties", "icon" => "icon-user", "route" => "properties.index"),
+    array("name" => "Users", "icon" => "lucide-user", "route" => "users.index"),
+    array("name" => "Appointments", "icon" => "lucide-calendar", "route" => "appointments.index"),
+    array("name" => "Partners", "icon" => "lucide-users", "route" => "partners.index"),
+);
+
+if (session()->has('user')) {
+    $links[] = array("name" => "Logout", "icon" => "lucide-log-out", "route" => "admin.logout");
+}
+?>
+
+<nav class="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-gray-700">
+    @foreach($links as $item)
+        <a href="#">
+            <div role="button" tabindex="0"
+                class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-100 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+                <div class="grid place-items-center mr-4">
+                    <i class="{{ $item['icon'] }} h-5 w-5"></i> <!-- Clases correctas de Lucide -->
+                </div>
+                {{ $item['name'] }}
+            </div>
+        </a>
+    @endforeach
 </nav>
+
