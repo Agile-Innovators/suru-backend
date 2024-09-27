6e4d73221b231fbe5af758fa1670e71c472dd931
ashleyrojas <ashleycr33@gmail.com>
2024-09-26 21:16:15 -0600

    feature: creating main controllers: user, partners, properties and appointments

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -17,7 +17,7 @@ if (session()->has('user')) {
             <div role="button" tabindex="0"
                 class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-100 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
                 <div class="grid place-items-center mr-4">
-                    <i class="{{ $item['icon'] }} h-5 w-5"></i> <!-- Clases correctas de Lucide -->
+                    <i class="{{ $item['icon'] }} h-5 w-5"></i> 
                 </div>
                 {{ $item['name'] }}
             </div>
