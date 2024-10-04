14016b8262ced8546ef4fa77fdfc10a3c9d2e668
ashleyrojas <ashleycr33@gmail.com>
2024-10-03 21:44:44 -0600

    changes in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,18 @@
 <?php
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 require __DIR__ . '/../public/index.php';
 require __DIR__ . '/../routes/api.php';
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
